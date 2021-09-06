04a674336709fc48860356e7b691cecd2697b400
Adrian Piech <adrianpiech.git@gmail.com>
2021-09-06 15:23:07 +0200

    Refactor: PureComponent on React.memo

--- src/components/layout/MainAreaComponent/MainAreaComponent.js
+++ src/components/layout/MainAreaComponent/MainAreaComponent.js
@@ -1,28 +1,21 @@
-import React, {PureComponent} from 'react';
+import React, {useEffect} from 'react';
 import Area from '../../views/RUNETERRA__MAP/area/Area';
 import styles from './MainAreaComponent.module.scss';
 import PropTypes from 'prop-types';
 
-class MainAreaComponent extends PureComponent {
+const MainAreaComponent = ({areas}) => {
 
-  /* WITH React Component: */
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   return this.props.areas === nextProps.areas ? false : true;
-  // }
+  useEffect(() => {console.log('rendered');});
 
-  render() {
-    const {areas} = this.props;
-
-    return (
-      <div className={styles.container}>
-        {areas.map(area => <Area key={area.id} {...area} />)}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.container}>
+      {areas.map(area => <Area key={area.id} {...area} />)}
+    </div>
+  );
+};
 
 MainAreaComponent.propTypes = {
   areas: PropTypes.array.isRequired,
 };
 
-export default MainAreaComponent;
+export default React.memo(MainAreaComponent);
